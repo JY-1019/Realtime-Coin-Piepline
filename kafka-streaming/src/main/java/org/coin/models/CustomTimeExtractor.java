94fd1ccdc47fd508a38a4571b2e66c04d75efa70
JY-1019 <76830616+JY-1019@users.noreply.github.com>
2024-03-26 14:44:28 +0900

    Update CustomTimeExtractor.java

--- kafka-streaming/src/main/java/org/coin/models/CustomTimeExtractor.java
+++ kafka-streaming/src/main/java/org/coin/models/CustomTimeExtractor.java
@@ -9,12 +9,9 @@ public class CustomTimeExtractor implements TimestampExtractor {
     public long extract(ConsumerRecord<Object, Object> record, long previousTimestamp) {
         try {
             JSONObject jsonObject = new JSONObject((String) record.value());
-            // 데이터에서 시간 정보를 추출합니다.
             long timestamp = jsonObject.getLong("timestamp");
-            // 밀리초 단위로 변환하여 반환합니다.
             return timestamp;
         } catch (Exception e) {
-            // 에러 발생 시 기본 타임스탬프를 반환합니다.
             return record.timestamp();
         }
     }
